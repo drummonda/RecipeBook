d7d0980ee9f0a208c718645b25e2f59cb5776ccb
Andrew Drummond <andrewdrummond@andrews-mbp.home>
2019-01-17 13:55:20 -0500

    updated components

--- src/app/header/recipe/recipe-list/recipe-list.component.ts
+++ src/app/header/recipe/recipe-list/recipe-list.component.ts
@@ -7,6 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe("Spaghetti", 
+							 "Delicious pasta", 
+							 "https://img.taste.com.au/hyE5dSN3/taste/2016/11/spaghetti-with-meatballs-and-spicy-tomato-sauce-102298-1.jpeg"
+							 ),
+		new Recipe("Spaghetti", 
+							 "Delicious pasta", 
+							 "https://img.taste.com.au/hyE5dSN3/taste/2016/11/spaghetti-with-meatballs-and-spicy-tomato-sauce-102298-1.jpeg"
+							 )
+	]
 
   constructor() { }
 
