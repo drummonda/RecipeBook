e2095058d00f973904e9da4b083d0cbca3d36140
Andrew Drummond <andrewdrummond@andrews-mbp.home>
2019-01-16 21:48:24 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeComponent } from './header/recipe/recipe.component';
+import { RecipeListComponent } from './header/recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './header/recipe/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './header/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './header/recipe/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipeComponent
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
--- src/app/header/recipe/recipe.component.ts
+++ src/app/header/recipe/recipe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
